5405336f6b29dd2024c2eceab15fda6086aa4b8e
Samuel Gunter <samathingamajig@gmail.com>
2025-03-29 04:07:42 -0500

    fix: don't download unused font

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -33,7 +33,7 @@ export const Route = createRootRoute({
                 crossOrigin: 'anonymous',
             },
             {
-                href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Roboto+Flex:opsz,wght@8..144,100;8..144,200;8..144,300;8..144,400;8..144,500;8..144,600;8..144,700;8..144,800;8..144,900;8..144,1000&display=swap',
+                href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
                 rel: 'stylesheet',
             },
             {
